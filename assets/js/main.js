0d15452469bb6c2f1ff8665900d27f8c89743e6e
adr <senhoraroldo@gmail.com>
2021-04-01 11:57:55 -0300

    feat :js carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ import * as config from './modules/swiper__props.js'; //Configs do Swiper
 import Dom from './modules/constructors.js'; //selecionar elementos
 import menu from './modules/menu.js'; //js do menu
 import * as animacao from './modules/animation.js';
+import * as carouselClick from './modules/carousel-change.js';
 
 menu(); //executa function do menu
 
@@ -10,6 +11,9 @@ menu(); //executa function do menu
 const pageHome = new Dom().el("#page__home")
 const pageEmpresas = new Dom().el("#page__empresas")
 const pageContato = new Dom().el("#page__contato")
+
+//empresas variantes
+const pageEmpresasVariante1 = new Dom().el(".variante__1")
 
 const pages = {
     pageHome,
@@ -24,8 +28,11 @@ function escopoPages() {
         new Dom().bodyClass("body__home")
         new Swiper(".swiper-container-1", config.props);
         new Swiper(".swiper-container-2", config.props);
+        new Swiper(".swiper-container-3", config.props);
+        new Swiper(".swiper-container-4", config.props);
+        new Swiper(".swiper-container-5", config.props);
         new Swiper(".swiper-container-fornecedores", config.props2);
-        new Swiper(".swiper-container-transportadoras", config.props);
+        new Swiper(".swiper-container-transportadoras", config.propstrans);
 
         // animação intro
         window.addEventListener("load", function () {
@@ -34,6 +41,7 @@ function escopoPages() {
             }
         })
         animacao.animaAoScroll()
+        carouselClick.carouselMuda()
 
 
     } else if (pageEmpresas) { // ★ Empresas
@@ -41,6 +49,12 @@ function escopoPages() {
         animacao.animacaoCirculo()
         animacao.animacaoIntroEmpresas()
         animacao.animaAoScroll()
+
+        if (pageEmpresasVariante1) {
+            new Dom().bodyClass("body__variante-1")
+            new Swiper(".swiper-container-1", config.props);
+
+        }
 
     } else if (pageContato) { // ★ Contato
         new Dom().bodyClass("body__contato")
--- assets/js/modules/swiper__props.js
+++ assets/js/modules/swiper__props.js
@@ -52,3 +52,58 @@ const props = {
         },
     }
 }
+
+
+export
+const propstrans = {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    centeredSlides: false,
+    speed: 600,
+    autoplay: {
+        delay: 500,
+        disableOnInteraction: true,
+    },
+    speed: 600,
+    autoplay: {
+        delay: 500,
+        disableOnInteraction: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: "auto",
+            centeredSlides: true,
+            spaceBetween: 15,
+
+        },
+        720: {
+            slidesPerView: "auto",
+            centeredSlides: false,
+            spaceBetween: 15,
+            autoplay: false,
+            speed: 0,
+
+
+        },
+        1024: {
+            slidesPerView: "auto",
+            spaceBetween: 50,
+            centeredSlides: false,
+            autoplay: false,
+            speed: 0,
+        },
+
+        1240: {
+            slidesPerView: "auto",
+            spaceBetween: 20,
+            centeredSlides: false,
+            autoplay: false,
+            speed: 0,
+
+        },
+    }
+}
